0cd4c0dc303c3b8365bd2f624f7e8ad20dd70f89
EgorDyach <tvoysosed33@gmail.com>
2025-01-24 16:57:03 +0300

    fix: hotfix

--- src/modules/rootPage/RootPage.tsx
+++ src/modules/rootPage/RootPage.tsx
@@ -1,14 +1,17 @@
 import ContentLoader from '@components/ContentLoader';
+import AppRoutes from '@lib/configs/routes';
 import { isAdmin } from '@lib/utils/isAdmin';
 import { uiSelectors } from '@store/ui';
 import { lazy, Suspense } from 'react';
 import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
 const LazyAdminPage = lazy(() => import('../adminPage/AdminPage'));
 const LazyEducationPage = lazy(() => import('../educationPage/EducationPage'));
 
 export const RootPage = () => {
     const user = useSelector(uiSelectors.getUser);
+    if (!user) return <Navigate to={AppRoutes.login} />;
     return (
         <Suspense fallback={<ContentLoader />}>
             {isAdmin(user) ? <LazyAdminPage /> : <LazyEducationPage />}
