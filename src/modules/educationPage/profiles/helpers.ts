629d8d4b7e6406d330ee6fa953aba30c31f75b65
EgorDyach <tvoysosed33@gmail.com>
2025-02-15 16:50:37 +0300

    hotfix

--- src/modules/educationPage/profiles/helpers.ts
+++ src/modules/educationPage/profiles/helpers.ts
@@ -1,8 +1,8 @@
 export const validateProfile = (
   newProfile: Profile,
-  shifts: Profile[],
+  profiles: Profile[],
 ): string | undefined => {
   if (!newProfile.name) return 'Необходимо ввести название профиля!';
-  if (shifts.map((el) => el.name).includes(newProfile.name))
+  if (profiles.map((el) => el.name).includes(newProfile.name))
     return 'Данный профиль существует!';
 };
