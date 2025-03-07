af4f965e4c0b4c3a2ad832124b1a1459c349f970
Van <MisterDeterminator@yandex.ru>
2025-03-07 18:24:32 +0000

    fix: room helpers

--- src/modules/educationPage/rooms/helpers.ts
+++ src/modules/educationPage/rooms/helpers.ts
@@ -2,14 +2,14 @@ export const validateRoom = (
   newRoom: Room,
   shifts: Room[],
 ): string | undefined => {
-  if (!newRoom.name) return 'Необходимо ввести название аудитории!';
+  if (!newRoom.room.name) return 'Необходимо ввести название аудитории!';
   if (
     shifts
-      .filter((el) => el.id !== newRoom.id)
-      .map((el) => el.name)
-      .includes(newRoom.name)
+      .filter((el) => el.room.id !== newRoom.room.id)
+      .map((el) => el.room.name)
+      .includes(newRoom.room.name)
   )
     return 'Данная аудитория существует!';
-  if (newRoom.capacity <= 0 || newRoom.capacity > 1000000)
+  if (newRoom.room.capacity <= 0 || newRoom.room.capacity > 1000000)
     return 'Некорректная вместимость аудитории!';
 };
